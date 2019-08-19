cb378fe1fe249dff7c8fe0d3c743292b5b909c4f
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2019-08-19 17:39:15 -0300

    Agregando Funcionalidad

--- Prototipo-1/Assets/Scrpts/CheckDamage.cs
+++ Prototipo-1/Assets/Scrpts/CheckDamage.cs
@@ -2,32 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shild : MonoBehaviour
+public class CheckDamage : MonoBehaviour
 {
     // Start is called before the first frame update
     public Enemy enemy;
-    public Player Player;
     public bool InEnemy;
+    public Player player;
     public bool InPlayer;
+
+    // Update is called once per frame
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Proyectil")
+        if (collision.gameObject.tag == "Proyectil")
         {
+            //ACA PROGRAMAR QUE LE BAJE VIDA TANTO AL ENEMIGO COMO AL JUGADOR DEPENDIENDO DE LOS BOOLEANOS YA DEFINIDOS
             Proyectil proyect = collision.GetComponent<Proyectil>();
-
-            if (InEnemy)
-            {
-                if (enemy.typeEnemy == Enemy.Categoria.Defensivo)
-                {
-                    Debug.Log("CONTRA ATAQUE");
-                    enemy.CounterAttack();
-                }
-            }
             if (proyect != null)
             {
                 proyect.GetPoolObject().Recycle();
             }
         }
-
     }
 }
