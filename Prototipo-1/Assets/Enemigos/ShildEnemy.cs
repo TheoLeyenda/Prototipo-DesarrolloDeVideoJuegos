3264730d923b705f72fdcefd0091fa2b6bde1e9c
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2019-09-10 13:26:03 -0300

    Organizando en carpetas

--- Prototipo-1/Assets/Enemigos/ShildEnemy.cs
+++ Prototipo-1/Assets/Enemigos/ShildEnemy.cs
@@ -2,26 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shild : MonoBehaviour
+public class ShildEnemy : MonoBehaviour
 {
     // Start is called before the first frame update
     public Enemy enemy;
-    public Player Player;
-    public bool InEnemy;
-    public bool InPlayer;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Proyectil")
         {
             Proyectil proyect = collision.GetComponent<Proyectil>();
-
-            if (InEnemy)
+            if (enemy.typeEnemy == Enemy.TiposDeEnemigo.Defensivo)
             {
-                if (enemy.typeEnemy == Enemy.TiposDeEnemigo.Defensivo)
-                {
-                    Debug.Log("CONTRA ATAQUE");
-                    enemy.CounterAttack(false);
-                }
+                Debug.Log("CONTRA ATAQUE");
+                enemy.CounterAttack(false);
             }
             if (proyect != null)
             {
