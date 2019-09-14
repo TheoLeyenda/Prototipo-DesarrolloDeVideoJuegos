2cd178372160dae4e40f9d46468768fe5c759eca
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2019-09-14 01:07:15 -0300

    Nuevo sistema de grillas ocupadas

---
+++ Prototipo-1/Assets/Elements Game/Jugador/Prototipo-2/SpritePlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototipo_2 {
+    public class SpritePlayer : MonoBehaviour
+    {
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            switch (collision.gameObject.tag)
+            {
+                case "Cuadrilla":
+                    Cuadrilla cuadrilla = collision.GetComponent<Cuadrilla>();
+                    cuadrilla.stateCuadrilla = Cuadrilla.StateCuadrilla.Ocupado;
+                    //Debug.Log("ENTRE");
+                    break;
+            }
+        }
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            switch (collision.gameObject.tag)
+            {
+                case "Cuadrilla":
+                    Cuadrilla cuadrilla = collision.GetComponent<Cuadrilla>();
+                    cuadrilla.stateCuadrilla = Cuadrilla.StateCuadrilla.Libre;
+                    //Debug.Log("SALI");
+                    break;
+            }
+        }
+    }
+}
