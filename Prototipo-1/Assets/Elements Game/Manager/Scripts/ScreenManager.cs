6965121b6b8cbf57db5a1deabc89439fc2ec22c8
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2019-09-19 19:34:22 -0300

    Empezando flow de niveles

--- Prototipo-1/Assets/Elements Game/Manager/Scripts/ScreenManager.cs
+++ Prototipo-1/Assets/Elements Game/Manager/Scripts/ScreenManager.cs
@@ -3,22 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ScreenManager : MonoBehaviour
-{
-    public void Prueba()
+namespace Prototipo_2 {
+    public class ScreenManager : MonoBehaviour
     {
-        SceneManager.LoadScene("SampleScene");
-    }
-    public void Supervivencia()
-    {
-        SceneManager.LoadScene("Supervivencia");
-    }
-    public void Historia()
-    {
-
-    }
-    public void Menu()
-    {
-        SceneManager.LoadScene("MENU");
+        private GameObject fondo;
+        private SpriteRenderer spriteRendererFondo;
+        public GameManager gm;
+        public List<Sprite> ListaNiveles;
+        private int idListaNiveles;
+        private void Start()
+        {
+            idListaNiveles = 0;
+        }
+        public void Prueba()
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+        public void Supervivencia()
+        {
+            SceneManager.LoadScene("Supervivencia");
+        }
+        public void Historia()
+        {
+            CheckMainCameraInScreen();
+        }
+        public void CheckMainCameraInScreen()
+        {
+            if (gm != null && SceneManager.GetActiveScene().name != "MENU" && SceneManager.GetActiveScene().name != "Supervivencia" 
+                && SceneManager.GetActiveScene().name != "SampleScene" && SceneManager.GetActiveScene().name != "GameOver")
+            {
+                if (fondo == null)
+                {
+                    fondo = GameObject.Find("Fondo");
+                    Debug.Log(fondo);
+                    spriteRendererFondo = fondo.GetComponent<SpriteRenderer>();
+                    Debug.Log(spriteRendererFondo);
+                    spriteRendererFondo.sprite = ListaNiveles[idListaNiveles];
+                    idListaNiveles++;
+                }
+            }
+        }
+        public void Menu()
+        {
+            SceneManager.LoadScene("MENU");
+        }
+        private void Update()
+        {
+            if (gm != null)
+            {
+                if (gm.enumsGameManager.modoDeJuego == EnumsGameManager.ModosDeJuego.Historia)
+                {
+                    Historia();
+                }
+            }
+        }
     }
 }
