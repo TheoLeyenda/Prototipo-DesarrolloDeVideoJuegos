6c173102560ec7ff5294d742e063e2497a4b0ec7
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2019-09-24 12:23:58 -0300

    Empezando el input para jostick

---
+++ Prototipo-1/Assets/Elements Game/Jugador/Prototipo-2/InputManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototipo_2 {
+    public class InputManager : MonoBehaviour
+    {
+        public Player player1;
+        public Player player2;
+        // Update is called once per frame
+        void Update()
+        {
+            CheckInputPlayer1();
+            CheckInputPlayer2();
+        }
+        public void CheckInputPlayer1()
+        {
+            if (InputPlayerController.Vertical_Button_P1() > 0)
+            {
+                player1.enumsPlayers.movimiento = EnumsPlayers.Movimiento.Saltar;
+            }
+            else if (InputPlayerController.Vertical_Button_P1() < 0)
+            {
+                player1.enumsPlayers.movimiento = EnumsPlayers.Movimiento.Agacharse;
+            }
+
+            if (InputPlayerController.Horizontal_Button_P1() < 0)
+            {
+                player1.enumsPlayers.movimiento = EnumsPlayers.Movimiento.MoverAtras;
+            }
+            else if (InputPlayerController.Horizontal_Button_P1() > 0)
+            {
+                player1.enumsPlayers.movimiento = EnumsPlayers.Movimiento.MoverAdelante;
+            }
+        }
+        public void CheckInputPlayer2()
+        {
+
+        }
+    }
+}
---
+++ Prototipo-1/Assets/Elements Game/Jugador/Prototipo-2/InputPlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputPlayerController
+{
+    public static float Vertical_Button_P1()
+    {
+        float r = 0.0f;
+        r += Input.GetAxis("Vertical_Button_P1");
+        return Mathf.Clamp(r, -1.0f, 1.0f);
+    }
+    public static float Horizontal_Button_P1()
+    {
+        float r = 0.0f;
+        r += Input.GetAxis("Horizontal_Button_P1");
+        return Mathf.Clamp(r, -1.0f, 1.0f);
+    }
+    public static Vector3 MainJostick_P1()
+    {
+        return new Vector3(Horizontal_Button_P1(), 0, Vertical_Button_P1());
+    }
+}
