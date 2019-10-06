3f31946d7c7c7b96ee21077393c4826e9d002dd2
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2019-10-05 22:54:58 -0300

    Esto tiene una pinta barbara

--- Prototipo-1/Assets/Elements Game/Seleccionador de Opciones/PVP/DataCombatPvP.cs
+++ Prototipo-1/Assets/Elements Game/Seleccionador de Opciones/PVP/DataCombatPvP.cs
@@ -9,16 +9,15 @@ namespace Prototipo_2 {
         // STRUCT GAME MANAGER
         public enum Player_Selected
         {
-            Nulo,
             Balanceado,
             Agresivo,
             Defensivo,
             Protagonista,
-            Count
+            Count,
+            Nulo,
         }
         public enum Level_Selected
         {
-            Nulo,
             Anatomia,
             Historia,
             EducacionFisica,
@@ -28,12 +27,21 @@ namespace Prototipo_2 {
             Programacion,
             TESIS,
             Cafeteria,
-            Count
+            Count,
+            Nulo,
         }
+        //LAS LISTAS DE Player1 y Player2 DEBEN SER INICIALIZADAs EN EL MISMO ORDEN QUE EL ENUMERADOR Player_Selected.
+        public List<GameObject> Players1;
+        public List<GameObject> Players2;
+
+        //LA LISTA levels DEBE SER INICIALIZADA EN EL MISMO ORDEN QUE EL ENUMERADOR Level_Selected.
+        public List<Sprite> levels;
+
         public SpriteRenderer fondoNivel;
         public Player_Selected player1_selected;
         public Player_Selected player2_selected;
         public Level_Selected level_selected;
+        
         private GameManager gm;
         void Start()
         {
@@ -48,21 +56,7 @@ namespace Prototipo_2 {
             player1_selected = gm.structGameManager.gm_dataCombatPvP.player1_selected;
             player2_selected = gm.structGameManager.gm_dataCombatPvP.player2_selected;
             level_selected = gm.structGameManager.gm_dataCombatPvP.level_selected;
-            switch (player1_selected)
-            {
-                case Player_Selected.Agresivo:
-                    Debug.Log("Elegi Agresivo");
-                    break;
-                case Player_Selected.Balanceado:
-                    Debug.Log("Elegi Balanceado");
-                    break;
-                case Player_Selected.Defensivo:
-                    Debug.Log("Elegi Defensivo");
-                    break;
-                case Player_Selected.Protagonista:
-                    Debug.Log("Elegi Protagonista");
-                    break;
-            }
+            Instantiate(Players1[(int)player1_selected]);
             switch (player2_selected)
             {
                 case Player_Selected.Agresivo:
@@ -78,36 +72,7 @@ namespace Prototipo_2 {
                     
                     break;
             }
-            switch (level_selected)
-            {
-                case Level_Selected.Anatomia:
-                    Debug.Log("Elegi aula anatomia");
-                    break;
-                case Level_Selected.Historia:
-                    Debug.Log("Elegi aula Historia");
-                    break;
-                case Level_Selected.EducacionFisica:
-                    Debug.Log("Elegi aula Educacion Fisica");
-                    break;
-                case Level_Selected.Arte:
-                    Debug.Log("Elegi aula Arte");
-                    break;
-                case Level_Selected.Matematica:
-                    Debug.Log("Elegi aula Matematica");
-                    break;
-                case Level_Selected.Quimica:
-                    Debug.Log("Elegi aula Quimica");
-                    break;
-                case Level_Selected.Programacion:
-                    Debug.Log("Elegi aula Programacion");
-                    break;
-                case Level_Selected.TESIS:
-                    Debug.Log("Elegi aula TESIS");
-                    break;
-                case Level_Selected.Cafeteria:
-                    Debug.Log("Elegi aula Cafeteria");
-                    break;
-            }
+            fondoNivel.sprite = levels[(int)level_selected];
         }
         // ESTE SCRIPT DEBE TOMAR LA DEDICION CORRESPONDIENTE EN CUANTO AL NIVEL ELEJIDO Y PERSONAJE ELEJIDO DEPENDIENDO DE LA INFO QUE LE PASE EL
         // STRUCT GAME MANAGER
