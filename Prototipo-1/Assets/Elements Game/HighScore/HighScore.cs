3289d150929ab2866b85d9231bec9a436b81cffb
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2019-10-22 17:57:38 -0300

    tocando un poquito el highscore

--- Prototipo-1/Assets/Elements Game/HighScore/HighScore.cs
+++ Prototipo-1/Assets/Elements Game/HighScore/HighScore.cs
@@ -2,67 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HighScore : MonoBehaviour
+namespace Prototipo_2
 {
-    // Start is called before the first frame update
+    public class HighScore : MonoBehaviour
+    {
+        // Start is called before the first frame update
 
-    [System.Serializable]
-    public class ScoreData
-    {
-        public string namePlayer;
-        public float scorePlayer;
-    }
-    // SERA UNA LISTA DE LOS MEJORES 10 PUNTAJES
-    public List<ScoreData> scores;
-    public List<ScoreData> initialHighScoreData;
-    public float scoreActual;
-    public string nameActual;
-    void Start()
-    {
-        LoadListScore();
-        CheckListScore();
-        SaveListScore();
-    }
-    public void LoadListScore()
-    {
-        for (int i = 0; i < scores.Count; i++)
+        [System.Serializable]
+        public class ScoreData
         {
-            scores[i].namePlayer = PlayerPrefs.GetString(scores[i].namePlayer, scores[i].namePlayer);
-            scores[i].scorePlayer = PlayerPrefs.GetFloat(scores[i].namePlayer, 0);
+            public string namePlayer;
+            public float scorePlayer;
         }
-    }
-    public void SaveListScore()
-    {
-        for (int i = 0; i < scores.Count; i++)
+        // SERA UNA LISTA DE LOS MEJORES 10 PUNTAJES
+        public List<ScoreData> scores;
+        public List<ScoreData> initialHighScoreData;
+        private GameManager gm;
+        public float scoreActual;
+        public string nameActual;
+        void Start()
         {
-            PlayerPrefs.SetString(scores[i].namePlayer, scores[i].namePlayer);
-            PlayerPrefs.SetFloat(scores[i].namePlayer, scores[i].scorePlayer);
-        }
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void CheckListScore()
-    {
-        int id = -1;
-        for (int i = 0; i < scores.Count; i++)
-        {
-            if(scoreActual > scores[i].scorePlayer)
+            if (GameManager.instanceGameManager != null)
             {
-                id = i;
+                gm = GameManager.instanceGameManager;
+            }
+            //USAR EL VIDEO DE "Input Feld erstellen - Text / Passwort richtig auslesen - Unity 2018" PARA INCORPORAR UNA INTERFAZ
+            //PARA QUE EL JUGADOR INGRESE SU NOMBRE.
+            scoreActual = gm.playerData_P1.score;
+            LoadListScore();
+            CheckListScore();
+            SaveListScore();
+        }
+        public void LoadListScore()
+        {
+            for (int i = 0; i < scores.Count; i++)
+            {
+                scores[i].namePlayer = PlayerPrefs.GetString(scores[i].namePlayer, scores[i].namePlayer);
+                scores[i].scorePlayer = PlayerPrefs.GetFloat(scores[i].namePlayer, 0);
             }
         }
-        if (id >= 0 && id < scores.Count)
+        public void SaveListScore()
         {
-            scores[id].namePlayer = nameActual;
-            scores[id].scorePlayer = scoreActual;
+            for (int i = 0; i < scores.Count; i++)
+            {
+                PlayerPrefs.SetString(scores[i].namePlayer, scores[i].namePlayer);
+                PlayerPrefs.SetFloat(scores[i].namePlayer, scores[i].scorePlayer);
+            }
         }
-    }
-    public void ResetScoreList()
-    {
-        scores.Clear();
-        scores = initialHighScoreData;
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        public void CheckListScore()
+        {
+            int id = -1;
+            for (int i = 0; i < scores.Count; i++)
+            {
+                if (scoreActual > scores[i].scorePlayer)
+                {
+                    id = i;
+                }
+            }
+            if (id >= 0 && id < scores.Count)
+            {
+                scores[id].namePlayer = nameActual;
+                scores[id].scorePlayer = scoreActual;
+            }
+        }
+        public void ResetScoreList()
+        {
+            scores.Clear();
+            scores = initialHighScoreData;
+        }
     }
 }
