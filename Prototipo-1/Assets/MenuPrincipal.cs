b2081e0cce10e65a47672431d30a4eaffebf9979
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2019-10-31 01:46:13 -0300

    Opcion de controles en la pantalla de menu principal

---
+++ Prototipo-1/Assets/MenuPrincipal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject menu;
+    public GameObject controles;
+
+
+    public void ActivateMenu()
+    {
+        menu.SetActive(true);
+        controles.SetActive(false);
+    }
+
+    public void ActivateControls()
+    {
+        menu.SetActive(false);
+        controles.SetActive(true);
+    }
+}
