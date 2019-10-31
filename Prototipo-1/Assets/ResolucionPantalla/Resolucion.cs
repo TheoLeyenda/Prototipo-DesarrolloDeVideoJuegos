ea1e448985a777a8203994b07525531c4c16f695
theoleyenda <theoleyenda1997@gmail.com>
2019-10-31 18:18:26 -0300

    escalador de pantalla incorporado

---
+++ Prototipo-1/Assets/ResolucionPantalla/Resolucion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resolucion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int Resolucion_X;
+    public int Resolucion_Y;
+    public bool fullScreen;
+    void Start()
+    {
+        Screen.SetResolution(Resolucion_X, Resolucion_Y, fullScreen);
+    }
+}
