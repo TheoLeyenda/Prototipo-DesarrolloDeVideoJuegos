21affa44cf9044f08fa97364653697f083d2b0d6
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2019-11-15 17:28:09 -0300

    configuraciones de pantalla

--- Prototipo-1/Assets/ResolucionPantalla/Resolucion.cs
+++ Prototipo-1/Assets/ResolucionPantalla/Resolucion.cs
@@ -5,18 +5,38 @@ using UnityEngine;
 public class Resolucion : MonoBehaviour
 {
     // Start is called before the first frame update
-    public int Resolucion_X;
-    public int Resolucion_Y;
+    public float Resolucion_X;
+    public float Resolucion_Y;
+    private float OriginalResolution_X;
+    private float OriginalResolution_Y;
     public bool fullScreen;
+    public bool automaticRescaledResolution;
     void Start()
     {
-        Screen.SetResolution(Resolucion_X, Resolucion_Y, fullScreen);
-        if (Screen.width != Resolucion_X && Screen.height != Resolucion_Y)
+        OriginalResolution_X = Resolucion_X;
+        OriginalResolution_Y = Resolucion_Y;
+
+        Screen.SetResolution((int)Resolucion_X, (int)Resolucion_Y, fullScreen);
+        if (automaticRescaledResolution)
         {
-            Screen.SetResolution(Resolucion_X / 2, Resolucion_Y / 2, fullScreen);
+            if (Screen.width < Resolucion_X && Screen.height < Resolucion_Y)
+            {
+                Resolucion_X = Resolucion_X / 1.2f;
+                Resolucion_Y = Resolucion_Y / 1.2f;
+                if (Screen.width < Resolucion_X && Screen.height < Resolucion_Y)
+                {
+                    Resolucion_X = OriginalResolution_X / 1.5f;
+                    Resolucion_Y = OriginalResolution_Y / 1.5f;
+                    if (Screen.width < Resolucion_X && Screen.height < Resolucion_Y)
+                    {
+                        Resolucion_X = OriginalResolution_X / 2f;
+                        Resolucion_Y = OriginalResolution_Y / 2f;
+                    }
+                }
+                Screen.SetResolution((int)Resolucion_X, (int)Resolucion_Y, fullScreen);
+            }
         }
         //Debug.Log(Screen.width + " "Screen.height);
-
     }
     
 }
