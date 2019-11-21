90fc3a70f8654d60153afaf1d5398b10ea6e712e
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2019-11-21 10:46:31 -0300

    cursor de event system fixeado

---
+++ Prototipo-1/Assets/CheckSelectedEventSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CheckSelectedEventSystem : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (!EventSystem.current.currentSelectedGameObject)
+        {
+            EventSystem.current.SetSelectedGameObject(EventSystem.current.firstSelectedGameObject);
+        }
+    }
+}
---
+++ Prototipo-1/Assets/MouseController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool HideMouse;
+    void Start()
+    {
+        LockedAndInvisibleCursor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            LockedAndInvisibleCursor();
+        }
+    }
+    public void LockedAndInvisibleCursor()
+    {
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        UnityEngine.Cursor.visible = false;
+    }
+}
