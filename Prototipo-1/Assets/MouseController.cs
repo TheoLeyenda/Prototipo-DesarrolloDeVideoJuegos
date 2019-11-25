40b63e97a30f75cae7941de1bbed5ff378966498
theixgamer <theofritaku@gmail.com>
2019-11-25 04:28:29 -0300

    nueva pantalla de inicio incorporada

--- Prototipo-1/Assets/MouseController.cs
+++ Prototipo-1/Assets/MouseController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MouseController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public bool HideMouse;
+    //public bool HideMouse;
     void Start()
     {
         LockedAndInvisibleCursor();
