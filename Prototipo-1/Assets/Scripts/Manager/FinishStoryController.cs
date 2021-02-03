7a6e49ff1d849f20029d536dafa0e8cecfa8d5cc
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2021-02-02 21:32:35 -0300

    Musica al finalizar el modo historia incorporada...

--- Prototipo-1/Assets/Scripts/Manager/FinishStoryController.cs
+++ Prototipo-1/Assets/Scripts/Manager/FinishStoryController.cs
@@ -7,6 +7,14 @@ public class FinishStoryController : MonoBehaviour
     // Start is called before the first frame update
     public Player player;
     public LevelManager levelManager;
+    private EventWise eventWise;
+    private bool musicPlaying;
+    private void Start()
+    {
+        musicPlaying = false;
+        eventWise = GameObject.Find("EventWise").GetComponent<EventWise>();
+    }
+
     private void OnEnable()
     {
         ProfesorEducacionFisica.FinishLevel += FinishStory;
@@ -19,5 +27,10 @@ public class FinishStoryController : MonoBehaviour
     {
         player.weitVictory = true;
         levelManager.ObjectiveOfPassLevel = 0;
+        if (!musicPlaying)
+        {
+            musicPlaying = true;
+            eventWise.StartEvent("pvp_ganador");
+        }
     }
 }
